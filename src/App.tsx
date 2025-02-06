da61b9ae8b5ef41f84b35850b859183ab85a553a
romain-sidambarompoulle <romain.sidambarompoulle@gmail.com>
2025-02-06 23:53:40 +0400

    Ajout d'un nouveau bouton

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import Home from "./pages/Home";
 import KineSimulator from "./pages/Simulator/KineSimulator";
