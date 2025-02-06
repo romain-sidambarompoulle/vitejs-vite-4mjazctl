8c26823bb5bbdf9be4748f366d7e38d2b69dd427
romain-sidambarompoulle <romain.sidambarompoulle@gmail.com>
2025-02-06 23:30:23 +0400

    Ajout d'un nouveau bouton

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy'; // Importez le plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // Configurez le plugin pour copier le fichier _redirects
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects', // Chemin du fichier _redirects dans le dossier public
+          dest: '.', // Dossier de destination (le dossier de build)
+        },
+      ],
+    }),
+  ],
   base: "/vitejs-vite-4mjazctl/", // Obligatoire pour GitHub Pages !
 });
