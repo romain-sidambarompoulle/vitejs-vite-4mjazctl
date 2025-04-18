2766996be6c17a9f078ae26df8e2ad6cd276d978
romain-sidambarompoulle <romain.sidambarompoulle@gmail.com>
2025-04-18 14:09:58 +0400

    CI: auto‑deploy front to GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import Home from "./pages/Home";
-import KineSimulator from "./pages/Simulator/KineSimulator";
-import SageFemmeSimulator from "./pages/Simulator/SageFemmeSimulator";
-import InfirmierSimulator from "./pages/Simulator/InfirmierSimulator";
-import InfirmierExplanation from "./pages/Explanation/InfirmierExplanation";
-import SagefemmeExplanation from "./pages/Explanation/SageFemmeExplanation";
-import KineExplanation from "./pages/Explanation/KineExplanation";
 import Contact from "./pages/Contact";
 import ReglesDeCalcul from "./pages/ReglesDeCalcul";
 import Navbar from "./components/Navbar";
 import FormulaireRDV from "./pages/FormulaireRDV";
-// import APropos from "./pages/Apropos"; // Import gardé commenté s’il sert plus tard
 import Simulateur from "./pages/Simulator/Simulateur";
 // import Chatbot from "./components/Chatbot"; // Chatbot temporairement désactivé
 
@@ -29,15 +22,12 @@ function App() {
         <Route path="/explanation/sage-femme" element={<SagefemmeExplanation />} />
         <Route path="/explanation/kine" element={<KineExplanation />} />
         <Route path="/contact" element={<Contact />} />
-        {/* <Route path="/a-propos" element={<APropos />} /> */}
         <Route path="/simulateur" element={<Simulateur />} />
         <Route path="/regles-de-calcul" element={<ReglesDeCalcul />} />
         <Route path="/simulateur/:type" element={<Simulateur />} />
         <Route path="/formulaire-rdv" element={<FormulaireRDV />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-
-      {/* <Chatbot />  <-- Composant Chatbot désactivé */}
     </Router>
   );
 }
